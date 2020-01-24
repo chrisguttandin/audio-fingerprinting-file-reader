5992bf2c5d345060972b92ea8731c6aa44026a9f
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-01-25 00:11:13 +0100

    test(config): run karma directly

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -4,17 +4,12 @@ module.exports = {
         'sh:build-es2018',
         'sh:build-es5'
     ],
-    continuous: [
-        'build',
-        'karma:continuous'
-    ],
     lint: [
         'sh:lint-config',
         'sh:lint-src',
         'sh:lint-test'
     ],
     test: [
-        'build',
-        'karma:test'
+        'sh:test-integration'
     ]
 };
